6ec9123b5fe6e8865f33ad60994531579a7c8c92
Alan Jamieson <Alan@sh-yose-7518.local>
2016-02-25 10:52:04 -0500

    Porgy Driver upload

---
+++ s16/Proj2Driver.py
@@ -0,0 +1,26 @@
+#Proj2Driver.py
+#Test Driver for COSC 251 Project 2
+#Spring 2016
+#Last Revision - 2/25/2016
+#Alan C. Jamieson
+
+#To run, make sure that your Proj2.py file is in the
+#same directory as this file.
+#Open this file in IDLE and Run Module (F5)
+
+import Proj2
+
+#Problem 1 - user input based
+Proj2.Problem1()
+
+#Problem 2
+#Expected output
+#THECO MMISS ARSAY SHELL OABCD
+#AIGIS AGREA TINVE STMEN TAAAA
+Proj2.Problem2("54123 OSYLD CSALC TMASO HMRHA EISEB")
+Proj2.Problem2("41532 IEVEA AATST SAENA GRNMA IGITA")
+
+#Problem 3
+#Expected output
+#Hello World!
+Proj2.Problem3("++++++++[>++++[>++>+++>+++>+<<<<-]>+>+>->>+[<]<-]>>.>---.+++++++..+++.>>.<-.<.+++.------.--------.>>+.>++.")
