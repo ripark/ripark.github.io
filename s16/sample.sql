ecf15ad3e46b5924e5be85067b0622b8d784ae42
Alan Jamieson <Alan@sh-yose-7518.smcm.edu>
2016-03-22 16:30:28 -0400

    sql adjustment

--- s16/sample.sql
+++ s16/sample.sql
@@ -1,3 +1,4 @@
+-- sample code derived from Brian Showalter
 SELECT datediff('2040-12-31','2010-01-01');
 
 CREATE DATABASE IF NOT EXISTS Calendar;
@@ -21,9 +22,9 @@ CREATE TABLE calendar_table (
 );
 
 CREATE TABLE ints ( i tinyint );
- 
+
 INSERT INTO ints VALUES (0),(1),(2),(3),(4),(5),(6),(7),(8),(9);
- 
+
 INSERT INTO calendar_table (dt)
 SELECT DATE('2010-01-01') + INTERVAL a.i*10000 + b.i*1000 + c.i*100 + d.i*10 + e.i DAY
 FROM ints a JOIN ints b JOIN ints c JOIN ints d JOIN ints e
@@ -43,14 +44,14 @@ SET isWeekday = CASE WHEN dayofweek(dt) IN (1,7) THEN 0 ELSE 1 END,
 	dayname = dayname(dt),
 	w = week(dt),
 	holidayDescr = '';
-    
+
 UPDATE calendar_table SET isHoliday = 1, holidayDescr = 'New Year''s Day' WHERE m = 1 AND d = 1;
- 
+
 UPDATE calendar_table c1
 LEFT JOIN calendar_table c2 ON c2.dt = c1.dt + INTERVAL 1 DAY
 SET c1.isHoliday = 1, c1.holidayDescr = 'Holiday for New Year''s Day'
 WHERE c1.dw = 6 AND c2.m = 1 AND c2.dw = 7 AND c2.isHoliday = 1;
- 
+
 UPDATE calendar_table c1
 LEFT JOIN calendar_table c2 ON c2.dt = c1.dt - INTERVAL 1 DAY
 SET c1.isHoliday = 1, c1.holidayDescr = 'Holiday for New Year''s Day'
@@ -71,12 +72,12 @@ WHERE m = 5 AND dw = 2 AND d BETWEEN 25 AND 31;
 UPDATE calendar_table
 SET isHoliday = 1, holidayDescr = 'Independence Day'
 WHERE m = 7 AND d = 4;
- 
+
 UPDATE calendar_table c1
 LEFT JOIN calendar_table c2 ON c2.dt = c1.dt + INTERVAL 1 DAY
 SET c1.isHoliday = 1, c1.holidayDescr = 'Holiday for Independence Day'
 WHERE c1.dw = 6 AND c2.m = 7 AND c2.d = 4 AND c2.dw = 7 AND c2.isHoliday = 1;
- 
+
 UPDATE calendar_table c1
 LEFT JOIN calendar_table c2 ON c2.dt = c1.dt - INTERVAL 1 DAY
 SET c1.isHoliday = 1, c1.holidayDescr = 'Holiday for Independence Day'
@@ -90,12 +91,12 @@ WHERE m = 9 AND dw = 2 AND d BETWEEN 1 AND 7;
 UPDATE calendar_table
 SET isHoliday = 1, holidayDescr = 'Veteran''s Day'
 WHERE m = 11 AND d = 11;
- 
+
 UPDATE calendar_table c1
 LEFT JOIN calendar_table c2 ON c2.dt = c1.dt + INTERVAL 1 DAY
 SET c1.isHoliday = 1, c1.holidayDescr = 'Holiday for Veteran''s Day'
 WHERE c1.dw = 6 AND c2.m = 11 AND c2.d = 11 AND c2.dw = 7 AND c2.isHoliday = 1;
- 
+
 UPDATE calendar_table c1
 LEFT JOIN calendar_table c2 ON c2.dt = c1.dt - INTERVAL 1 DAY
 SET c1.isHoliday = 1, c1.holidayDescr = 'Holiday for Veteran''s Day'
@@ -112,15 +113,13 @@ WHERE m = 11 AND dw = 6 AND d BETWEEN 21 AND 29;
 UPDATE calendar_table
 SET isHoliday = 1, holidayDescr = 'Christmas Day'
 WHERE m = 12 AND d = 25;
- 
+
 UPDATE calendar_table c1
 LEFT JOIN calendar_table c2 ON c2.dt = c1.dt + INTERVAL 1 DAY
 SET c1.isHoliday = 1, c1.holidayDescr = 'Holiday for Christmas Day'
 WHERE c1.dw = 6 AND c2.m = 12 AND c2.d = 25 AND c2.dw = 7 AND c2.isHoliday = 1;
- 
+
 UPDATE calendar_table c1
 LEFT JOIN calendar_table c2 ON c2.dt = c1.dt - INTERVAL 1 DAY
 SET c1.isHoliday = 1, c1.holidayDescr = 'Holiday for Christmas Day'
 WHERE c1.dw = 2 AND c2.m = 12 AND c2.d = 25 AND c2.dw = 1 AND c2.isHoliday = 1;
-
-
