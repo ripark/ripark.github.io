88180c729b8cf28d36e5d819c2b06a262e6c3763
Alan Jamieson <Alan@sh-yose-7518.local>
2017-02-05 01:16:00 -0500

    stubs

---
+++ s17/Stack.h
@@ -0,0 +1,17 @@
+#include "Student.h"
+
+class Stack{
+
+public:
+  Stack(){}
+  int length(){return 0;}
+  void printStack(){}
+  void push(Student element){}
+  Student top(){Student s; return s;}
+  void pop(){}
+  void sort(){}
+  void clear(){}
+  Student topandpop(){Student s; return s;}
+  bool isEmpty(){return true;}
+  int size(){return 0;}
+};
